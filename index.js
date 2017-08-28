cb3d2234aecf879651e6249efd1e8f24de7e49d1
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-28 19:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+function happyHolidayTo(name, holiday) {
+  return `Happy ${holiday}, ${name}!`
+}
+
 function holidayCountdown(holidayName, numberOfDays) {
   return `It's ${days} days until ${holidayName}`
 }
