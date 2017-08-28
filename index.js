dc27d7cc58ee39dcba034882f15eb035fdf3cc7a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-28 18:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}"
+  return "Happy holidays, ${name}" (name)
 }
 
 console.log(happyHolidaysTo("Ryan"))
